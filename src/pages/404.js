c59739ecf45eff0a60e7898364a6fe8858f58586
Martin Munro <mmunro@ltrr.arizona.edu>
2020-07-31 09:11:57 -0700

    Make a trivial change to give us something to push.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import InteriorPage from "../components/interiorpage"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <InteriorPage>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <p>Sorry, you just hit a route that doesn&#39;t exist.</p>
+  </InteriorPage>
 )
 
 export default NotFoundPage
