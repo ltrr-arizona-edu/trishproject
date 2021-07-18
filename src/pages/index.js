381d171c6b077dac9fb6fd9fea00034ba8e04140
rthaxton <87099514+rthaxton@users.noreply.github.com>
2021-07-18 15:51:33 -0700

    Update Homepage Text in index.js
    Fixed grammar/typos on lines 27, 29, 33, 40, 49, 50, 53-54

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,20 +24,20 @@ const IndexPage = () => (
           <p>
             Modern observations of Arctic river flow, and estimates of
             associated heat budgets, rarely extend back more than several
-            decades. These records are too short and discontinues to fully
+            decades. These records are too short and discontinuous to fully
             quantify the long-term trends in freshwater flow and heat
-            budgets. This makes difficult to understand the processes
+            budgets. This makes it difficult to understand the processes
             underlying those trends.
           </p>
           <p>
-            TRISH project is designed to fill that gap by utilizing
+            The TRISH project is designed to fill that gap by utilizing
             tree-ring records and river gauge data in the Arctic to
             reconstruct and study multi-century fluctuations of water, heat
             and energy flow to the Arctic Ocean.
           </p>
           <p>
             We use a wide array of existing and newly collected tree-ring
-            data in Siberia, coupled with a long record of river gauge data,
+            data from Siberia, coupled with a long record of river gauge data,
             to develop and test reconstructions of water flow and heat
             budgets for the Yenisei River, the second largest river of the
             Arctic.
@@ -46,12 +46,12 @@ const IndexPage = () => (
             Causal attribution of spatial-temporal variations of river
             hydrology requires consideration of changing snowmelt, thawing
             permafrost, and modification of natural flow regimes by dams and
-            reservoirs. This consideration is assessed with the New
-            Hampshire University Water Balance Model. With the engagement of
+            reservoirs. These considerations are addressed with the New
+            Hampshire University Water Balance Model. By engaging with
             multiple layers of instrumental and modeled data updated to 2020
             (e.g., climatic, hydrologic, anthropogenic, General Circulation
-            Model (GCM) outputs) the team is using over 300 tree-ring
-            records from Siberia to reconstruct river discharge and water
+            Model (GCM) outputs) the team is using over 300 Siberian tree-ring
+            records to reconstruct river discharge and water
             temperature for the last 300 years.
           </p>
         </div>
